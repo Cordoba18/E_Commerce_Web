33a68624162eb44b8775e0c335ffcac2bcfa8357
KarlaR7 <123266431+KarlaR7@users.noreply.github.com>
2023-06-05 19:04:11 -0500

    funcion

---
+++ SoxStyle_WEB/controlador/carrito_controller.php
@@ -0,0 +1,14 @@
+<?php 
+function productos(){
+    $produc = array(
+        "Nombre"  => "valor1",
+        "Descripcion"  => "valor2",
+        "Precio"  => "valor3",
+        
+    );
+
+
+}
+
+
+?>
--- SoxStyle_WEB/vista/carrito de compras/CarritoCompras.php
+++ SoxStyle_WEB/vista/carrito de compras/CarritoCompras.php
@@ -1,17 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head> 
-     <meta charset="UTF-8">
-     <meta name="viewport" content="width=device-width, initial-scale=1.0.">
-     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-     <title>Carrito de compras</title>
-     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
-     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
-     <link rel="stylesheet" href="style.css">
-    </head>
+<head> 
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0.">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Carrito de compras</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.6/dist/umd/popper.min.js" integrity="sha384-oBqDVmMz9ATKxIep9tiCxS/Z9fNfEXiDAYTujMAeBAsjFuCZSmKbSSUnQlmh/jp3" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.min.js" integrity="sha384-cuYeSxntonz0PPNlHhBs68uyIAVpIIOZZ5JqeqvYYIcEL727kskC66kF92t6Xl2V" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="style.css">
+</head>
 
-     <body>
+    <body>
         <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
             <a class="navbar-brand">Sox Style</a>
             <button class="navbar-toggler" data-target="#my-nav" data-toggle="collapse" aria-controls="my-nav" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,6 +25,8 @@
                 </ul>
             </div>
         </nav>
+        <!-- FIN DEL NAVBAR -->
+
         <br>
         <br>
         <br>
@@ -36,60 +38,20 @@
                         <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Favoritos()</a>
             </li>
         </ul>
-       <div class="row" >
+
+        <div class="row" >
         <div class= "col-3">
-         <div class="card">
+        <div class="card">
             <img class="card-img-top" src="/proyecto/E_Commerce_Web/imagenes/imagen.jpg" alt="">
             <div class="card-body">
+                <h4 >Nombre</h4>
                 <h5 class="card-title">Descripcion</h5>
                 <p class="card-text">precio</p>
                 <button class="btn btn-primary" value="agregar" name="btnAccion" type="submit">Favoritos</button>
             </div>
-         </div>
-         </div>
-         <div class= "col-3">
-         <div class="card">
-            <img class="card-img-top" src="/proyecto/E_Commerce_Web/imagenes/g2.webp" alt="">
-            <div class="card-body">
-                <h5 class="card-title">Descripcion </h5>
-                <p class="card-text">Precio</p>
-                <button class="btn btn-primary" value="agregar" name="btnAccion" type="submit">Favoritos</button>
-            </div>
-         </div>
-         </div>
-         <div class= "col-3">
-         <div class="card">
-            <img class="card-img-top" src="/proyecto/E_Commerce_Web/imagenes/camisa.webp" alt="">
-            <div class="card-body">
-                <h5 class="card-title">Descripcion</h5>
-                <p class="card-text">precio</p>
-                <button class="btn btn-primary" value="agregar" name="btnAccion" type="submit">Favoritos</button>
-            </div>
-         </div>
-         </div>
-         <div class= "col-3">
-         <div class="card">
-            <img class="card-img-top" src="/proyecto/E_Commerce_Web/imagenes/camisa2.jpg" alt="">
-            <div class="card-body">
-                <h5 class="card-title">Descripcion</h5>
-                <p class="card-text">Precio</p>
-                <button class="btn btn-primary" value="agregar" name="btnAccion" type="submit">Favoritos</button>
-            </div>
-         </div>
-         </div>
-         <div class= "col-3">
-         <div class="card">
-            <img class="card-img-top" src="/proyecto/E_Commerce_Web/imagenes/gorra.webp" alt="">
-            <div class="card-body">
-                <h5 class="card-title">Descripcion</h5>
-                <p class="card-text">precio</p>
-                <button class="btn btn-primary" value="agregar" name="btnAccion" type="submit">Favoritos</button>
-            </div>
-         </div>
-         </div>
-         <br>
-         <br>
-         <h2>Total:</h2>
-         <button class="btn btn-primary"  value="comprar" name="btnAccion" type="submit">Comprar</button>
-     </body>
+        <br>
+        <br>
+        <h2>Total:</h2>
+        <button class="btn btn-primary"  value="comprar" name="btnAccion" type="submit">Comprar</button>
+    </body>
 </html>
