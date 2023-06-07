c39d6fdb0624d55bf3862f40e26dd1c9100c9c93
Juanmanuelx24 <juanmanuelx242@gmail.com>
2023-06-06 22:29:11 -0500

    hola
    aqui esta la lista de deseos con css y ahi deje la base de datos de prueba.

--- SoxStyle_WEB/vista/V-Lista_deseos/listadeseos.php
+++ SoxStyle_WEB/vista/V-Lista_deseos/listadeseos.php
@@ -8,7 +8,11 @@
     //exit;
     //}
 ?>
-
+<!-- este codigo es de la base de datos de prueba --> /
+<?php
+    $connect = mysqli_connect("localhost","root",""     ,"lista_deseos");
+?> 
+<!--  -->
 <!DOCTYPE html>
 <html lang="es">
     <head> 
@@ -16,80 +20,71 @@
      <meta name="viewport" content="width=device-width, initial-scale=1.0.">
      <meta http-equiv="X-UA-Compatible" content="ie=edge">
      <title>Lista de deseos</title>
+
+     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
      <link rel="stylesheet" href="./style.css">
+     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css">
+
     </head>
-     <body>
-            <img class="img" src="./SoxStyle.jpg" >
-            <br>
-            <hr>
-            <a>
-                <h3>Sox Style</h3>
-            </a>
-            <div>
-                <ul>
-                    <h3>Carrito</h3> 
-                    <h3>Favoritos:()</h3>
-                </ul>
+     <body>  
+             <div class="contenedor">
+                <img src="./img/SoxStyle.jpg" alt="" class="image">
             </div>
+               <div class="container">
+                    <h3 class="text-left" id="carrito">Carrito:()</h3>
+                    <h3 class="text-left">Favoritos:()</h3>
+                </div>
+
             <div class="content-select">
-	            <select class="left">
+	            <select class="select">
 		            <option>En ofertas </option>
 		            <option>Añadidos recientemente</option>
 		            <option>Orden alfabetico</option>
                     <option>Precio</option>
 	            </select>
-	            <i></i>
+	            
             </div>
             <br>
-            <br>
-            <div  class="contenedor">
-                <div class="columna">
-                    <h2>Nombre del producto</h2>
-                    <p>$ XXXXXXX</p>
-                    <button class="button-9" role="button">Quitar</button>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <circle cx="6" cy="19" r="2" />
-                    <circle cx="17" cy="19" r="2" />
-                    <path d="M17 17h-11v-14h-2" />
-                    <path d="M6 5l14 1l-1 7h-13" />
-                    </svg>
-                    
-                    <a href="../carrito de compras/CarritoCompras.php">Carrito de compras</a>
-                </div>
-
-                <div class="columna">
-                    <h2>Nombre del producto</h2>
-                    <p>$ XXXXXXX</p>
-                    <button class="button-9" role="button">Quitar</button>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <circle cx="6" cy="19" r="2" />
-                    <circle cx="17" cy="19" r="2" />
-                    <path d="M17 17h-11v-14h-2" />
-                    <path d="M6 5l14 1l-1 7h-13" />
-                    </svg>
-                    
-                    <a href="../carrito de compras/CarritoCompras.php">Carrito de compras</a>
-                </div>
-
-                <div class="columna">
-                    <h2>Nombre del producto</h2>
-                    <p>$ XXXXXXX</p>
-                    <button class="button-9" role="button">Quitar</button>
-                    <svg xmlns="http://www.w3.org/2000/svg" class="icon icon-tabler icon-tabler-shopping-cart" width="44" height="44" viewBox="0 0 24 24" stroke-width="1.5" stroke="#2c3e50" fill="none" stroke-linecap="round" stroke-linejoin="round">
-                    <path stroke="none" d="M0 0h24v24H0z" fill="none"/>
-                    <circle cx="6" cy="19" r="2" />
-                    <circle cx="17" cy="19" r="2" />
-                    <path d="M17 17h-11v-14h-2" />
-                    <path d="M6 5l14 1l-1 7h-13" />
-                    </svg>
-                    <a href="../carrito de compras/CarritoCompras.php">Carrito de compras</a>
-                </div>
+            <br>    
+            <div class="card-header text-center">
+                   <p>PRODCUTOS
+                   </p>
             </div>
-            <a href="../Principal_producto/principal_p.html">Principal</a>
             <br>
-            <br>
-        <hr>
-     </body>
+        <table>
+            <thead>
+                <tr>
+                    <th>Referencia</th>
+                    <th>Producto</th>
+                    <th>Precio</th>
+                    <th>Acción</th>
+                </tr>
+            </thead>
+            <tbody>
+                <!-- esta es la base de datos de prueba -->
+                <?php
+                $query = "SELECT * FROM list_deseo";
+                $result = mysqli_query($connect, $query);
+
+                while ($row = mysqli_fetch_array($result)) {
+                    $imagen = $row['imagen'];
+                    $nombre_producto = $row['nombre_producto'];
+                    $precio = $row['precio'];
+
+                    echo "<tr>";
+                    echo "<td><img src='img/$imagen' alt='Imagen del producto'></td>";
+                    echo "<td>$nombre_producto</td>";
+                    echo "<td>$$precio</td>";
+                    echo "<td class='accion-column'><button class='eliminar-button' data-producto-id='$nombre_producto'>Eliminar</button><a href='carrito.php' class='carrito-icon'><i class='fas fa-shopping-cart'></i></a></td>";
+                    echo "</tr>";
+                }
+                ?>
+                <!--  -->
+            </tbody>
+        </table>
+        
+    </body>
+    <br>
+    <div class="card-header text-center">
+            </div>
 </html>
