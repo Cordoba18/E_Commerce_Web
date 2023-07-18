90841591f86dcc9c77a128c046ff22f75e688683
Victor Perlaza <victorperlaza11b@gmail.com>
2023-07-18 17:14:01 -0500

    Html
    agregue mis componentes html a mis vistas

--- Style_Sport_Laravel/resources/views/shopping/purchaseConfirmation.blade.php
+++ Style_Sport_Laravel/resources/views/shopping/purchaseConfirmation.blade.php
@@ -6,5 +6,48 @@
 
 <h1>confirmacion de compra</h1>
 
+<div class="container">
+    <h1>Lista de productos</h1>
+
+    <div class="table-container">
+      <table>
+        <tr>
+          <td class="t1">Camisa verde</td>
+          <td class="t2">$*******</td>
+        </tr>
+        <tr>
+          <td class="t1">Camisa roja</td>
+          <td class="t2">$*******</td>
+        </tr>
+        <tr>
+          <td class="t1">Camisa negra</td>
+          <td class="t2">$*******</td>
+        </tr>
+        <tr>
+          <td class="tot">Total</td>
+          <td class="t2">$*******</td>
+        </tr>
+      </table>
+    </div>
+  </div>
+  <div class="linea-horizontal"></div>
+
+
+      </div>
+      <br>
+      <br>
+      <div class="conta2">
+      <form action="" method="post">
+          <label for="Texto contrasena">Por favor ingrese la contraseña para realizar la compra</label>
+          <br>
+          <br>
+          <label class="txt2" for="contrasena">Contraseña</label>
+          <input class="campos2" type="text" name="contrasena">
+          <br>
+          <br>
+          <button type="submit" name="comprar" >Comprar</button>
+      </form>
+      </div>
+
 @endsection
 
--- Style_Sport_Laravel/resources/views/shopping/purchaseForm.blade.php
+++ Style_Sport_Laravel/resources/views/shopping/purchaseForm.blade.php
@@ -2,9 +2,69 @@
 
 @section('title', 'formulario compras')
 
+@section('css')
+@vite(['resources/css/app.scss'])
+@endsection
 @section('content')
 
 <h1>Formulario de compras</h1>
+
+<div class="container">
+    <form action="" method="post">
+    <label for="nombre" class="txt">Nombre</label>
+    <input type="text" name="nombre" class="campos">
+    <br>
+    <br>
+    <label for="Contacto" class="txt">Contacto</label>
+    <input type="text" name="contacto" class="campos">
+    <br>
+    <br>
+    <label for="Correo" class="txt">Correo</label>
+    <input type="text" name="correo" class="campos">
+    <br>
+    <br>
+    <label for="Direccion" class="txt">Direccion</label>
+    <input type="text" name="direccion" class="campos">
+</div>
+
+<div class="C2">
+    <br>
+    <br>
+    <h2>Metodos de pago </h2>
+
+    <div class="card">
+       <div class="card-title">Metodo #1</div>
+         <div class="card-content">
+          <p class="tit">Entidad Bancaria<span class="wis"></span>**********</p>
+          <p class="tit">Numero de Tarjeta<span class="wis"></span>**********</p>
+      </div>
+    </div>
+    <input type="checkbox" class="ch">
+    <br>
+    <br>
+    <div class="form-group">
+    <label for="Cuotas a pagar">Cuotas a pagar:</label>
+    <input type="text" name="cuotas">
+    </div>
+    <br><br>
+
+    <div class="card-container">
+     <div class="card-custom">
+       <div class="card-title">Agregar Nuevo Método de Pago</div>
+         <div class="card-content">
+    <div class="card-content-text">
+    </div>
+    </div>
+    </div>
+     <div class="card-custom" style="flex: 0.1;">
+    </div>
+     </div>
+     <br><br>
+
+    <button type="submit" name="continuar" class="btn">Continuar</button>
+</div>
+</form>
+
 
 @endsection
 
