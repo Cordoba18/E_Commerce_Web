565eb27a37a39769511a321e757a49550438ca2f
Jhan Carlos <jccq12@gmail.com>
2023-09-15 20:49:33 -0500

    debug

--- Style_Sport_Laravel/resources/views/layaouts/main.blade.php
+++ Style_Sport_Laravel/resources/views/layaouts/main.blade.php
@@ -31,7 +31,7 @@
 </body>
 <script>
     var logo = "{{ asset('storage/imgs/icon/Logo.png') }}";
-    var ruta_descarga = "{{ asset('storage/apk/app-release.apk') }}";
+    var ruta_descarga = "{{ asset('storage/apk/app-debug.apk') }}";
 </script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"
     integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous">
